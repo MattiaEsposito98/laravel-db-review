667957b2d2eed3cb7597c1a4678efae56eee274c
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-27 17:42:30 +0100

    add function index

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
     <h1>Homepage</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($clients as $client)
+                <ul>
+                    <li>Nome:{{ $client->name }} <br>
+                        Email: {{ $client->email }} <br>
+                        Posts: <ol>
+                            @if ($client->posts->isNotEmpty())
+                                <ol>
+                                    @foreach ($client->posts as $post)
+                                        <li>{{ $post->title }}</li>
+                                    @endforeach
+                                </ol>
+                            @else
+                                <li>Nessun post correlato</li>
+                            @endif
+                </ul>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.homepage');
-})->name('homepage');
+Route::get('/', [ClientController::class, 'index'])->name('homepage');
+
 
 Route::get('/about', function () {
     return view('pages.about');
@@ -14,3 +14,5 @@ Route::get('/contact', function () {
     $contacts = ['Mattia', 'Genny', 'Emanuele', 'Andrea', 'Pippo', 'Luigi'];
     return view('pages.contact', compact('contacts'));
 })->name('contact');
+
+// Route::resource('/posts', ClientController::class);
