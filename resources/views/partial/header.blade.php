55b73b75929e62135d6062ac130aca4f14dbdc71
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-27 16:41:09 +0100

    add rotte per la navbar

---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li>Home</li>
+    <li>Contact</li>
+    <li>Show</li>
+    <li><a href={{ route('about') }}>About</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/contact', function () {
+    $contact = ['Mattia', 'Genny', 'Emanuele', 'Andrea', 'Pippo', 'Luigi'];
+    return view('pages.contact', compact('contact'));
+})->name('contact');
